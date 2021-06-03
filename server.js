89dc9b083fc96a11c23d9a170894f9c1c3e930e9
angelicavc7 <angelicavillar_c@live.com>
2021-06-03 14:10:19 -0700

    folders

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// var allRoutes = require('./controllers');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// app.use(cors({
+//     origin:["herokuapp.com"]
+// }));
+
+// app.use('/',allRoutes);
 
 // app.use(express.static(path.join(__dirname, 'public'))); // static assets in public folder for passing in front end js and css
 
