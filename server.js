3725f5f8378d7dacede74aa1972715cdeb7ff725
Juan Vazquez <ehulises28@hotmail.com>
2021-06-04 16:15:46 -0700

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const sequelize = require("./config/connection");
 const express = require("express");
 const allRoutes = require("./controllers");
-
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(allRoutes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
