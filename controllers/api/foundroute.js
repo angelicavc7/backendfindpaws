ed0f306094f764e953189b59d7d413518cab14b1
Juan Vazquez <ehulises28@hotmail.com>
2021-06-08 12:35:52 -0700

    added delete and edit routes

--- controllers/api/foundroute.js
+++ controllers/api/foundroute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Found } = require("../../models");
+const { Found, Lost } = require("../../models");
 //gets all forms for pets
 router.get("/", (req, res) => {
   Found.findAll()
@@ -42,6 +42,54 @@ router.post("/", (req, res) => {
   })
     .then((foundPet) => {
       res.json(foundPet);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "error", err });
+    });
+});
+// edit found Pet info
+router.put("/:id", (req, res) => {
+  Found.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then(() => {
+    Found.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+        petType: req.body.petType,
+        petDescription: req.body.petDescription,
+        circumstances: req.body.circumstances,
+        dateFound: req.body.dateFound,
+        lastSeen: req.body.lastSeen,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then((editFoundPet) => {
+        res.json(editFoundPet);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "error", err });
+      });
+  });
+});
+//deletes found pet
+router.delete("/:id", (req, res) => {
+  Found.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((FoundPet) => {
+      res.json(FoundPet);
     })
     .catch((err) => {
       console.log(err);
